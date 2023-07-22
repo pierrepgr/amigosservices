9fbcc00e48871e1000771f6e9fd0a5506f9c43e0
Marcelo R. Pierre <marcelo.junior_ext@glintt.com>
2023-07-22 11:29:51 +0100

    :construction: Created customer pipeline

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -3,11 +3,16 @@ package com.amigoscode.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {
         "com.amigoscode.notification",
         "com.amigoscode.amqp"})
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
 
     public static void main(String[] args) {
